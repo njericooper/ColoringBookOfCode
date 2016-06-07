36d49afce4a660f720e64fd66f8e5616c412ee5f
njericooper <CLOUD901-Admin@CLOUD901Admins-MacBook-Pro-2.local>
2016-06-07 13:59:08 -0500

    Coloring Book Of Code
    This reverts commit 19592a5897808126e039b23bc37e6ad4d0ca1228.

--- Mult-line_If_Elseif_Statement.php
+++
@@ -1,7 +0,0 @@
-<?
-  if ($page == "Home") echo "You selected Home";
-  elseif ($page == "About") echo "You selected About";
-  elseif ($page == "News") echo "You selected News";
-  elseif ($page == "Login") echo "You selected Login";
-  elseif ($page == "Links") echo "You selected Links";
-?>  
