19592a5897808126e039b23bc37e6ad4d0ca1228
njericooper <CLOUD901-Admin@CLOUD901Admins-MacBook-Pro-2.local>
2016-06-07 13:55:38 -0500

    Progress

---
+++ Alt_Switch_Statement.php
@@ -0,0 +1,12 @@
+<?php
+  switch ($page):
+    case "Home":
+      echo "You selected Home";
+      break;
+
+  //ect...
+    case"Links":
+      echo "You selected Links";
+      break;
+  endswitch;
+?>
