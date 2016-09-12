321f4c17eef26e28ac368613cc0fc61c9611b414
njericooper <njericooper@gmail.com>
2016-09-12 10:16:56 -0500

    Updates

---
+++ 3_str_funct.php
@@ -0,0 +1,5 @@
+<?php
+  echo strrev (" .dlrow olleH"); // reverse string
+  echo str_repeat("Hip ", 2); // repeat string
+  echo strtoupper("hooray!"); // string to uppercase
+  ?>
---
+++ returning_values.php
@@ -0,0 +1,7 @@
+<?php
+  $lowered = strtolower("aNY # of Letters and Punctuation you WANT");
+  echo $lowered; // lowercase
+
+  $ucfixed = ucfirst("any # of letters and punctuation you want");
+  echo $ucfixed;
+ ?>
